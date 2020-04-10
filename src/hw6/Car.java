bc49b0e534d96abce133753ed0ca839073f05d7f
Roman A. Orlov <roor0717@netcracker.com>
2020-04-11 01:01:07 +0300

    hw6.1

--- src/hw6/Car.java
+++ src/hw6/Car.java
@@ -1,5 +1,8 @@
 package hw6;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     static {
@@ -8,17 +11,21 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private CountDownLatch cdl;
+    private CyclicBarrier barrier;
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CountDownLatch cdl, CyclicBarrier barrier) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cdl = cdl;
+        this.barrier = barrier;
     }
     @Override
     public void run() {
@@ -26,11 +33,13 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            barrier.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        cdl.countDown();
     }
 }
